aae6cd04ecdd546d57684d36282f0449b0e6f5e4
Nullsuke <nullsuke@gmail.com>
2020-10-31 21:35:57 +0900

    スコア、最大連鎖数、消去したぷよの数の表示

---
+++ Script/PlayDataRenderer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayDataRenderer : MonoBehaviour
+{
+    [SerializeField] Text scoreText = default;
+    [SerializeField] Text maxChainText = default;
+    [SerializeField] Text vanishedText = default;
+
+    public void Render(PlayData pd)
+    {
+        scoreText.text = pd.Score.ToString();
+        maxChainText.text = pd.MaxChain.ToString();
+        vanishedText.text = pd.Vanished.ToString();
+    }
+}

05df710abd9e3c1b9dd5d9626ed5de55b2610fc0
Nullsuke <nullsuke@gmail.com>
2020-10-31 14:50:19 +0900

    ぷよの移動関係

---
+++ Script/SingleMover.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+
+public class SingleMover : BaseMover
+{
+    public override bool CanMove()
+    {
+        var x = Mathf.RoundToInt(transform.position.x);
+        var cy = Round4thDecimalPlace(transform.position.y);
+        var y = Mathf.FloorToInt(cy);
+
+        if (!field.IsInside(x, y)) return false;
+
+        if (field[x, y] != null) return false;
+
+        return true;
+    }
+
+    //小数第4位で四捨五入
+    private float Round4thDecimalPlace(float n)
+    {
+        return (float)(Math.Round(n * 1000, MidpointRounding.AwayFromZero)) / 1000;
+    }
+}

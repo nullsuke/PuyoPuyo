74976915d84e1e597fc28dce0ccde110ea8c8330
Nullsuke <nullsuke@gmail.com>
2020-10-31 14:37:01 +0900

    移動関係の基底クラス

---
+++ Script/BaseMover.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public abstract class BaseMover : MonoBehaviour
+{
+    protected static Field field;
+    protected static int width;
+    protected static int height;
+    private static readonly float dropSpeed = 0.9f;
+
+    //PairMoverとSingleMoverで処理が違う
+    public abstract bool CanMove();
+
+    public void Setup(Field field)
+    {
+        BaseMover.field = field;
+        width = field.Width;
+        height = field.Height;
+    }
+
+    public void Fall(float speed)
+    {
+        transform.position += new Vector3(0, -speed, 0);
+    }
+
+    public void HardDrop()
+    {
+        var vec = new Vector3(0, -dropSpeed);
+
+        do
+        {
+            transform.position += vec;
+        } while (CanMove());
+
+        AdjustToAbove();
+    }
+
+    //上へずらす
+    public void AdjustToAbove()
+    {
+        var p = transform.position;
+        int y = Mathf.FloorToInt(p.y + 1);
+
+        transform.position = new Vector3(p.x, y);
+    }
+}

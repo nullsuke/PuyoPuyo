8b9d3adff472d881c02251a6ea7811b59e301ebc
Nullsuke <nullsuke@gmail.com>
2020-10-31 21:25:40 +0900

    スコア、最大連鎖数、消去したぷよの数の管理

---
+++ Script/PlayData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayData
+{
+    public int Score { get; private set; }
+    public int MaxChain { get; private set; }
+    public int Vanished { get; private set; }
+
+    public int UpdateData(List<Puyo> vanishable, int chainCount)
+    {
+        Score += Scorer.CountScore(vanishable, chainCount);
+        MaxChain = Mathf.Max(MaxChain, chainCount);
+        Vanished += vanishable.Count;
+
+        return Score;
+    }
+
+    public void Clear()
+    {
+        Score = 0;
+        MaxChain = 0;
+        Vanished = 0;
+    }
+}

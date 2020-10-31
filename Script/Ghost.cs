03c821961fe6e4b81e44a784b9ff8f7a7d395f1a
Nullsuke <nullsuke@gmail.com>
2020-10-31 14:39:55 +0900

    ゴースト関係

---
+++ Script/Ghost.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    private Puyo puyo;
+    private SingleMover mover;
+
+    public int IntX { get => Mathf.RoundToInt(transform.position.x); }
+    public int IntY { get => Mathf.RoundToInt(transform.position.y); }
+
+    public void Setup(Puyo puyo, Field field)
+    {
+        this.puyo = puyo;
+        mover.Setup(field);
+    }
+
+    //実体と同じ位置に移動
+    public void Move()
+    {
+        transform.position = puyo.transform.position;
+    }
+
+    public void HardDrop()
+    {
+        mover.HardDrop();
+    }
+
+    public void Destroy()
+    {
+        Destroy(gameObject);
+    }
+
+    private void Awake()
+    {
+        mover = GetComponent<SingleMover>();
+    }
+}
